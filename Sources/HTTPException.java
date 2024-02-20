346f5b4297d57b7ed69b6dbe8a54d2aba38657fe
Liel Huri <liel.huri@post.runi.ac.il>
2024-02-20 18:36:08 +0200

    Added HttpException class

---
+++ Sources/HTTPException.java
@@ -0,0 +1,11 @@
+public class HTTPException extends Exception
+{
+    private final String m_ErrorMessage;
+    private final int m_ErrorCode;
+
+    public HTTPException(String i_ErrorMessage, int i_ErrorCode) {
+        super();
+        m_ErrorMessage = i_ErrorMessage;
+        m_ErrorCode = i_ErrorCode;
+    }
+}
