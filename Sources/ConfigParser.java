2b551fec4a8ac7e8bda42118fdb434c939db9836
Yali Gal <yaligal1212@gmail.com>
2024-02-21 17:14:19 +0200

    configParser and first main

--- Sources/ConfigParser.java
+++ Sources/ConfigParser.java
@@ -1,32 +1,36 @@
-public class ConfigParser 
-{
-    private static String s_Port;
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class ConfigParser {
+    private static int s_Port;
     private static String s_Root;
     private static String s_DefaultPage;
     private static int s_MaxThreads;
 
-    public static void parseConfigFile(String i_PathToFile) 
-    {
-
+    public static void parseConfigFile(String i_PathToFile) throws Exception {
+        Properties properties = new Properties();
+        FileInputStream fileInputStream = new FileInputStream(i_PathToFile);
+        properties.load(fileInputStream);
+        s_Port = Integer.parseInt(properties.getProperty("port"));
+        s_Root = properties.getProperty("root");
+        s_DefaultPage = properties.getProperty("defaultPage");
+        s_MaxThreads = Integer.parseInt(properties.getProperty("maxThreads"));
+        fileInputStream.close();
     }
 
-    public static String getPort()
-    {
+    public static int getPort() {
         return s_Port;
     }
-    
-    public static String getRoot()
-    {
+
+    public static String getRoot() {
         return s_Root;
     }
-    
-    public static String getDefaultPagePath()
-    {
+
+    public static String getDefaultPagePath() {
         return s_DefaultPage;
     }
-    
-    public static int getMaxThreadNumber()
-    {
+
+    public static int getMaxThreadNumber() {
         return s_MaxThreads;
     }
 }
--- Sources/TCPServer.java
+++ Sources/TCPServer.java
@@ -4,6 +4,34 @@ import java.util.concurrent.Executors;
 
 public class TCPServer {
     public static void main(String[] args) {
-        
+
+        try {
+            ConfigParser.parseConfigFile("config.ini");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+
+        int port = ConfigParser.getPort();
+        int maxThreads = ConfigParser.getMaxThreadNumber();
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            ExecutorService executor = Executors.newFixedThreadPool(maxThreads);
+            System.out.println("Server is running on port " + port);
+            while (true) {
+                try {
+                    Socket clientSocket = serverSocket.accept();
+                    executor.execute(new TCPConnectionHandler(clientSocket));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Server is shutting down");
+        }
+
     }
 }
